d81fd38a0eb4c8140e5b8ad496cf9420cdc4d5dc
joseriaux <joseriaux@hotmail.fr>
2022-02-21 14:46:57 +0100

    ajout du contact

---
+++ Desktop/C#/Contact/Contact/Contact/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact
+{
+    class Contact
+    {
+
+        #region Attributs
+        private string _Lastname;
+        private int _Identifier;
+        private string _Firstname;
+        private int _Phone;
+        private string _Email;
+        private DateTime _BirthDay;
+        private int _ContactedCount;
+        #endregion
+
+        #region propriété
+
+        public string LastName
+        {
+            get { return _Lastname; }
+        }
+
+        public int Identifier
+        {
+            get { return _Identifier; }
+            set { _Identifier = value; }
+        }
+
+        public int Phone
+        {
+            get { return _Phone; }
+            set { _Phone = value; }
+        }
+
+        public string Firstname
+        {
+            get { return _Firstname; }
+            set { _Firstname = value; }
+        }
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+
+        public DateTime BirthDay
+        {
+            get { return _BirthDay; }
+            set { _BirthDay = value; }
+        }
+
+        public int ContactedCount
+        {
+            get { return _ContactedCount; }
+            set { _ContactedCount = value; }
+        }
+
+
+
+
+
+
+        #endregion
+
+        #region Construteur
+        public Contact(string nom)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Desktop/C#/Contact/Contact/Contact/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact1 = new Contact("der");
+            Contact contact2 = new Contact("dad");
+            Contact contact3 = new Contact("kji");
+            Contact contact4 = new Contact("fez");
+            Contact contact5 = new Contact("pz");
+            Contact contact6 = new Contact("dez");
+            Contact contact7 = new Contact("def");
+
+            List<Contact> list = new List<Contact>();
+
+            String choix;
+            do
+            {
+                Console.WriteLine("1 - Création d'un Contact");
+                Console.WriteLine("2 - Gestion de la liste des contacts");
+                Console.WriteLine("3 - Rechercher un contact");
+                Console.WriteLine("4 - Quitter l'application");
+                choix = Console.ReadLine();
+
+                Console.Clear();
+                switch (choix)
+                {
+                    case "1":
+                        Console.WriteLine("Création d'un Contact");
+                        Console.Read();
+                        break;
+                    case "2":
+                        Console.WriteLine("Gestion de la liste des contacts");
+                        Console.Read();
+                        break;
+                    case "3":
+                        Console.WriteLine("Rechercher un contact");
+                        Console.Read();
+                        break;
+                    case "4":
+                        Console.WriteLine("Quitter l'application");
+                        Console.Read();
+                        break;
+                        //default:
+                        //    Console.WriteLine("Choix inexistant");
+                        //    Console.Read();
+                        //    break;
+                }
+            } while (choix != "0");
+        }
+    }
+}
